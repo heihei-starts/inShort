393871c0dcd9a733c120b00b5730e140c3a58cac
heihei-starts <ryohei.reon.0819@icloud.com>
2021-10-21 20:18:36 +0900

    [add] 単語追加API作成(未テスト)

--- Backend/WordManageApi/wordManage.py
+++ Backend/WordManageApi/wordManage.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+
 from flask_cors import CORS
 from dotenv import load_dotenv
 
@@ -8,7 +9,7 @@ import sys
 sys.path.append(os.path.abspath(".."))
 
 #modules呼び出し
-from modules import manageSql 
+from modules import wordManageSql
 
 
 #ステータスコード 
@@ -18,14 +19,14 @@ HTTP_BAD_REQUEST = 400
 app = Flask(__name__)
 
 #CORS
-CORS(app, prigins=["http://localhost:8080"])
+CORS(app, origins=["http://localhost:8080"])
 
 #単語取得API
 @app.route("/", methods=['GET'])
 def get_words():
 
     #sqlオブジェクト呼び出し
-    sqlClass = manageSql.ManageSql()
+    sqlClass = wordManageSql.WordManageSql()
     #全件取得
     try:
         query = "SELECT word_name,field_id from in_short.words; "
@@ -45,6 +46,39 @@ def get_words():
         #コネクション接続解除
         sqlClass.closeConnection()
     
+
+
+#単語追加API
+@app.route("/", methods=['POST'])
+def post_word():
+    
+
+    #wordManagesqlオブジェクト呼び出し
+    sqlClass = wordManageSql.WordManageSql()
+
+    #リクエスト取得
+    data = request.get_json(force=True)
+
+    #リクエストデータ取り出す
+    word_name = data.get('word_name', None)
+    field_id  = data.get('field_id', None)
+
+    #単語追加
+    try:
+        query = "INSERT INTO in_short.words (word_name, field_id) VALUES (%s,%s);"
+        result = sqlClass.insert_word(word_name, field_id)
+
+        body = {'message': '単語追加完了'}
+        return jsonify(body), HTTP_OK
+
+    except Exception as e:
+        print("Exception error post_word")
+        print(e)
+    
+    finally:
+        #コネクション接続解除
+        sqlClass.closeConnection()
+
 
 
 if __name__ == "__main__":
