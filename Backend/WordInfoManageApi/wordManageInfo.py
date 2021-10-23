aa48806a7c49d1ea0e82d21b48ea8d1978610d60
heihei-starts <ryohei.reon.0819@icloud.com>
2021-10-23 17:03:30 +0900

    [add] 解説投稿API作成

--- Backend/WordInfoManageApi/wordManageInfo.py
+++ Backend/WordInfoManageApi/wordManageInfo.py
@@ -39,3 +39,42 @@ def get_words_info():
 
     finally:
         sqlClass.closeConnection()
+
+    #return
+
+
+#単語解説追加
+@app.route("/", methods=['POST'])
+def post_word_info():
+
+    #リクエストを受ける
+    data = request.get_json(force=True)
+
+    #リクエストデータ取り出し
+    explanation = data.get('explanation', None)
+    word_id     = data.get('word_id', None)
+    user_id     = data.get('user_id', None)
+
+    #sqlオブジェクト呼び出し
+    sqlClass= wordInfoManageSql.WordInfoManageSql()
+
+    #解説追加
+    try:
+        query = "INSERT INTO in_short.explanation('explanations', 'word_id', 'user_id')"
+
+        result = sqlClass.post_word_info(query, user_id, word_id, explanation)
+    except Exception as e:
+        print("Exception error post_word_info")
+        print(e)
+
+    finally:
+        sqlClass.closeConnection()
+
+    #return
+
+
+
+
+if __name__ == ""__main__:
+    app.run(debug=True, port=5001)
+
